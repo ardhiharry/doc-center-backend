c534048a964242cad6dd869ba958ea0b08f332be
Ardhi Harry <ardhiharry@gmail.com>
2025-05-07 14:33:34 +0700

    fix: change message in  endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'status' => 200,
-        'message' => 'Welcome to the Document Center API',
+        'message' => 'Welcome to the HMA Project Manager API',
         'data' => [],
         'pagination' => [],
         'errors' => []
